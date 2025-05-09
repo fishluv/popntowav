4bc57761abe3036d5f0583471b3eb3bd2ed04a97
fishluv <110374653+fishluv@users.noreply.github.com>
2025-05-09 01:43:49 -0500

    support more args and do fancy arg parsing

--- popntowav.js
+++ popntowav.js
@@ -4,6 +4,7 @@ const path = require("node:path");
 
 const LibSampleRate = require('@alexanderolsen/libsamplerate-js');
 const wav = require("wav");
+const yargs = require("yargs");
 
 const MSADPCM = require("./msadpcm");
 const Popnchart = require("./popnchart");
@@ -23,82 +24,124 @@ const diffLongToShort = {
     ex: "op",
 };
 
-function usageAndExit() {
-    console.log("Usage: node popntowav ifs_file [--easy|--normal|--hyper|--ex] [output_file]");
-    process.exit();
+const argv = yargs(process.argv.slice(2))
+    .usage("Usage: node $0 [-i <ifs_file_or_dir> [--keep-ifs-dir] [-d <difficulty>]] [--bin-file <bin_file> --2dx-file <2dx_file>] [<output_file>]")
+    .options({
+        "i": {
+            alias: "ifs-file-or-dir",
+            describe: "ifs file or dir to use. If file is provided, ifstools will be used to extract it. Must not be used with bin file and 2dx file options.",
+            type: "string",
+        },
+        "k": {
+            alias: "keep-ifs-dir",
+            describe: "Ignored unless ifs file is provided. By default, extracted ifs dir is deleted after processing. If this option is present, deletion will be skipped.",
+            type: "boolean",
+        },
+        "d": {
+            alias: "difficulty",
+            describe: "Ignored unless ifs file or dir is provided. Specifies which chart to use. Defaults to normal.",
+            choices: ["easy", "normal", "hyper", "ex"],
+        },
+        "b": {
+            alias: "bin-file",
+            describe: ".bin chart file to use. Must be used with 2dx file option.",
+            type: "string",
+        },
+        "t": {
+            alias: "2dx-file",
+            describe: ".2dx sample archive file to use. Must be used with bin file option.",
+            type: "string",
+        },
+    })
+    .conflicts("i", ["b", "t"])
+    .implies("b", "t")
+    .implies("t", "b")
+    .example([
+        ["-- IFS MODE"],
+        ["$0 -i v.ifs", "Extract and render V using its normal chart."],
+        ["$0 -i v.ifs /path/v.wav", "You can specify an output path at the end."],
+        ["$0 -i v.ifs -d ex", "Re-extract and render using the ex chart instead."],
+        ["$0 -i v.ifs -k", "Keep the ifs dir for future renders."],
+        ["$0 -i v.ifs", "This time extraction will be skipped since the ifs dir already exists."],
+        ["$0 -i v_ifs/", "You can also provide the ifs dir path."],
+        ["-- BIN/2DX MODE"],
+        ["$0 -b v_np.bin -t v.2dx", "You can also skip the ifs business altogether by specifying the .bin and .2dx files directly."],
+    ])
+    .hide("version")
+    .alias("h", "help")
+    .parse();
+
+if (argv.ifsFileOrDir && !fs.existsSync(argv.ifsFileOrDir)) {
+    console.log("specified ifs file or dir not found");
+    process.exit(1);
 }
-
-if (process.argv.length < 3) {
-    usageAndExit();
+if (argv.binFile && !fs.existsSync(argv.binFile)) {
+    console.log("specified bin file not found");
+    process.exit(1);
 }
-
-const ifsFile = process.argv[2];
-const ifsName = ifsFile.slice(0, -4); // Remove `.ifs` suffix
-
-let difficulty, outputFile;
-if (process.argv.length === 3) {
-    difficulty = "normal";
-    outputFile = `${ifsName}_${difficulty}.wav`;
-    console.log(`ifs file: ${ifsFile}`);
-    console.log("difficulty not specified so defaulting to: normal");
-    console.log(`output file not specified so defaulting to: ${outputFile}`);
+if (argv["2dxFile"] && !fs.existsSync(argv["2dxFile"])) {
+    console.log("specified 2dx file not found");
+    process.exit(1);
 }
-
-if (process.argv.length === 4) {
-    if (["--easy", "--normal", "--hyper", "--ex"].includes(process.argv[3])) {
-        difficulty = process.argv[3].slice(2);
-        outputFile = `${ifsName}_${difficulty}.wav`;
-        console.log(`ifs file: ${ifsFile}`);
-        console.log(`difficulty: ${difficulty}`);
-        console.log(`output file not specified so defaulting to: ${outputFile}`);
-    } else {
-        difficulty = "normal";
-        outputFile = process.argv[3];
-        console.log(`ifs file: ${ifsFile}`);
-        console.log("difficulty not specified so defaulting to: normal");
-        console.log(`output file: ${outputFile}`);
-    }
-}
-
-if (process.argv.length === 5) {
-    if (["--easy", "--normal", "--hyper", "--ex"].includes(process.argv[3])) {
-        difficulty = process.argv[3].slice(2);
-        outputFile = process.argv[4];
-        console.log(`ifs file: ${ifsFile}`);
-        console.log(`difficulty: ${difficulty}`);
-        console.log(`output file: ${outputFile}`);
-    } else {
-        usageAndExit();
-    }
-}
-
-if (process.argv.length > 5) {
-    usageAndExit();
-}
-
-child_process.execSync(`ifstools ${ifsFile}`);
-
-const ifsDir = `${ifsName}_ifs`;
-
-const availableCharts =
-    Object.entries(diffShortToLong)
-        .filter(([short, _]) => fs.existsSync(path.join(ifsDir, `${ifsName}_${short}.bin`)))
-        .map(([_, long]) => long);
-if (!availableCharts.includes(difficulty)) {
-    console.log(`ifs file contains no ${difficulty} chart. available charts: ${availableCharts.join(", ")}`);
-    fs.rmSync(ifsDir, {recursive: true});
+if (!argv.ifsFileOrDir && (!argv.binFile || !argv["2dxFile"])) {
+    console.log("must specify (ifs_file_or_dir) OR (bin_file AND 2dx_file)");
     process.exit(1);
 }
 
-const twodxFile = path.join(ifsDir, `${ifsName}.2dx`);
-const chartFile = path.join(ifsDir, `${ifsName}_${diffLongToShort[difficulty]}.bin`);
+const difficulty = argv.difficulty || "normal";
+
+let dirToCleanUp;
+let twodxFile;
+let chartFile;
+let outputFile = argv._[0];
+
+if (argv.ifsFileOrDir) {
+    const ifsName = path.basename(argv.ifsFileOrDir).slice(0, -4); // Remove `.ifs` or `_ifs` suffix
+
+    let ifsDir;
+    if (fs.statSync(argv.ifsFileOrDir).isDirectory()) {
+        ifsDir = argv.ifsFileOrDir;
+    } else {
+        ifsDir = `${ifsName}_ifs`;
+        if (fs.existsSync(ifsDir)) {
+            console.log("ifs dir already exists. reusing and will not delete afterwards.")
+        } else {
+            child_process.execSync(`ifstools ${argv.ifsFileOrDir}`);
+            if (!argv.keepIfsDir) {
+                dirToCleanUp = ifsDir;
+            }
+        }
+    }
+
+    const availableCharts =
+        Object.entries(diffShortToLong)
+            .filter(([short, _]) => fs.existsSync(path.join(ifsDir, `${ifsName}_${short}.bin`)))
+            .map(([_, long]) => long);
+
+    if (!availableCharts.includes(difficulty)) {
+        console.log(`ifs file contains no ${difficulty} chart. available charts: ${availableCharts.join(", ")}`);
+        if (!argv.keepIfsDir) {
+            fs.rmSync(ifsDir, {recursive: true});
+        }
+        process.exit(1);
+    }
+
+    twodxFile = path.join(ifsDir, `${ifsName}.2dx`);
+    chartFile = path.join(ifsDir, `${ifsName}_${diffLongToShort[difficulty]}.bin`);
+    outputFile ||= `${ifsName}_${difficulty}.wav`;
+} else {
+    if (argv.difficulty) {
+        console.log("ignoring specified difficulty because .bin and .2dx files were specified");
+    }
+    twodxFile = argv["2dxFile"];
+    chartFile = argv.binFile;
+    outputFile ||= `${path.basename(chartFile, ".bin")}.wav`;
+}
 
 let soundContainer = new Twodx(twodxFile);
 let chart = new Popnchart(chartFile, !soundContainer.late_bg);
 //The sound container is full of MSADPCM keysounds, so each one needs decoded.
 let decodedKeysounds = soundContainer.keysounds.map((keysound) => MSADPCM.decodeKeysoundOut(keysound.data, keysound.unk2));
-
-fs.rmSync(ifsDir, {recursive: true});
 
 let highestSample = 0;
 //Outputting stereo 44.1Khz regardless.
@@ -181,7 +224,12 @@ for (var i = 0; i<finalBuffer.length; i += 4) {
 let filename = soundContainer.name;
 filename = filename.slice(0, filename.indexOf("\u0000"));
 
+console.log(`outputting to ${outputFile}`);
 //I could manually generate a wav header, but I don't because I'm lazy.
 let writer = new wav.FileWriter(outputFile, {bitDepth: 32});
 writer.write(finalBuffer);
+
+if (dirToCleanUp) {
+    fs.rmSync(dirToCleanUp, {recursive: true});
+}
 });
